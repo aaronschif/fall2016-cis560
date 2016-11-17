522a7c68b4ea8512a4ac5cf7bef63de75d574ba1
Robert Stewart <rstewar2@ksu.edu>
2016-11-16 18:00:08 -0600

    Created user server

--- wowdb/sql.py
+++ wowdb/sql.py
@@ -7,6 +7,6 @@ def get_sql(filename):
 
 
 def create_tables():
-    with psycopg2.connect(host='localhost', user='wowdb') as conn:
+    with psycopg2.connect(host='138.197.18.7', user='wowdb') as conn:
         with conn.cursor() as cur:
             cur.execute(get_sql('create_tables.sql'))
