6182a5c42287473c8703d57ce2dae25472288ce8
Robert Stewart <rstewar2@ksu.edu>
2016-11-16 19:15:03 -0600

    Created skeleton for populating tables with conflict management

--- wowdb/sql/populate_tables.sql
+++ wowdb/sql/populate_tables.sql
@@ -1,30 +1,40 @@
 insert into faction (name)
 values ('Alliance'),
-       ('Horde');
+       ('Horde')
+on conflict do nothing;
 
 insert into race (name, racial_trait_1, racial_trait_2)
-values ();
+values ()
+on conflict do nothing;
 
 insert into class (name)
-values ();
+values ()
+on conflict do nothing;
 
 insert into specialization (class_name, specialization_name)
-values ();
+values ()
+on conflict do nothing;
 
 insert into gear (name, primary_stat, secondary_stat, material, tradable)
-values ();
+values ()
+on conflict do nothing;
 
 insert into "set" (name, set_bonus)
-values ();
+values ()
+on conflict do nothing;
 
 insert into location (map_region)
-values ();
+values ()
+on conflict do nothing;
 
 insert into dungeon (dungeon_name, boss)
-values ();
+values ()
+on conflict do nothing;
 
 insert into raid (raid_name, boss)
-values ();
+values ()
+on conflict do nothing;
 
 insert into vendor (first_name, surname, price)
-values ();
+values ()
+on conflict do nothing;
