c7f5e98f7d3157134dc830498947f4b6576fb41d
swimi <caseypoole@ksu.edu>
2016-11-28 19:34:55 -0600

    Working version of populate_tables - not all values are correct in this version

--- wowdb/sql/populate_tables.sql
+++ wowdb/sql/populate_tables.sql
@@ -4,13 +4,13 @@ values (1, 'Alliance'),
 on conflict do nothing;
 
 insert into race (id, name, racial_trait_1, racial_trait_2)
-values (1, 'Human'),
-       (2, 'Dwarf'),
-       (3, 'Night Elf'),
-       (4, 'Gnome'),
-       (5, 'Draenei'),
-       (6, 'Worgen'),
-       (7, 'Orc'),
+values (1, 'Human', '1', '1'),
+       (2, 'Dwarf', '1', '1'),
+       (3, 'Night Elf', '1', '1'),
+       (4, 'Gnome', '1', '1'),
+       (5, 'Draenei', '1', '1'),
+       (6, 'Worgen', '1', '1'),
+       (7, 'Orc', '1', '1'),
        (8, 'Undead', 'Will of the Foresaken', 'Cannibalize'),
        (9, 'Tauren', 'Brawn', 'War Stomp'),
        (10, 'Troll', 'berserking', 'Da Voodoo Shuffle'),
@@ -60,23 +60,22 @@ on conflict do nothing;
 
    I put notes next to each insert value to note where they come from.
  */
-insert into gear (id, name, slot, primary_stat, secondary_stat, material, tradable)
-values (1, 'Rook Footman''s Warboots', 'Feet', '556 Strength', '243 Critical Strike', 'Plate', 'No'), /* Black Rook Hold, Amalgam of Souls */
-       (2, 'Shadowfeather Shawl', 'Back', '417 Agility', '389 Haste', 'None', 'No'), /* Black Rook Hold, Amalgam of Souls */
-       (3, 'Etheldrin''s Breastplate', 'Chest', '742 Strenth', '705 Mastery', 'Plate', 'No'), /* Black Rook Hold, Amalgam of Souls */
-       (4, 'Ravencourt Formal Robes', 'Chest', '742 Intellect', '367 Critical Strike', 'Cloth', 'No'), /* Black Rook Hold, Amalgam of Souls */
-       (5, 'Soulstarve Hood', 'Head', '742 Agility', '599 Haste', 'Leather', 'No'), /* Black Rook Hold, Illysanna Ravencrest */
-       (6, 'Soul-Torn Fury Cinch', 'Waist', '556 Intellect', '259 Critical Strike', 'Mail', 'No'), /* Black Rook Hold, Illysanna Ravencrest */
-       (7, 'Slippers of Heedless Sacrifice', 'Feet', '556 Intellect', '275 Haste', 'Cloth', 'No'), /* Black Rook Hold, Illysanna Ravencrest */
-       (8, 'Leadfoot Earthshakers', 'Feet', '556 Strength', '227 Versatility', 'Plate', 'No'), /* Black Rook Hold, Smashspite the Hateful */
-       (9, 'Shorn Batbrood Cuffs', 'Wrist', '417 Agility', '397 Critical Strike', 'Leather', 'No'), /* Black Rook Hold, Smashspite the Hateful */
-       (10, 'Felbat Leather Vest', 'Chest', '742 Agility', '670 Critical Strike', 'Leather', 'No'), /* Black Rook Hold, Smashspite the Hateful */
-       (11, 'Mardum Chain Vest', 'Chest', '742 Intellect', '388 Versatility', 'Mail', 'No'), /* Black Rook Hold, Smashspite the Hateful */
-
+insert into gear (id, name, slot, primary_stat, secondary_stat_1, secondary_stat_2, material, tradable)
+values (1, 'Rook Footman''s Warboots', 'Feet', '556 Strength', '243 Critical Strike', '1', 'Plate', 'No'), /* Black Rook Hold, Amalgam of Souls */
+       (2, 'Shadowfeather Shawl', 'Back', '417 Agility', '389 Haste', '1', 'None', 'No'), /* Black Rook Hold, Amalgam of Souls */
+       (3, 'Etheldrin''s Breastplate', 'Chest', '742 Strenth', '705 Mastery', '1', 'Plate', 'No'), /* Black Rook Hold, Amalgam of Souls */
+       (4, 'Ravencourt Formal Robes', 'Chest', '742 Intellect', '367 Critical Strike', '1', 'Cloth', 'No'), /* Black Rook Hold, Amalgam of Souls */
+       (5, 'Soulstarve Hood', 'Head', '742 Agility', '599 Haste', '1', 'Leather', 'No'), /* Black Rook Hold, Illysanna Ravencrest */
+       (6, 'Soul-Torn Fury Cinch', 'Waist', '556 Intellect', '259 Critical Strike', '1', 'Mail', 'No'), /* Black Rook Hold, Illysanna Ravencrest */
+       (7, 'Slippers of Heedless Sacrifice', 'Feet', '556 Intellect', '275 Haste', '1', 'Cloth', 'No'), /* Black Rook Hold, Illysanna Ravencrest */
+       (8, 'Leadfoot Earthshakers', 'Feet', '556 Strength', '227 Versatility', '1', 'Plate', 'No'), /* Black Rook Hold, Smashspite the Hateful */
+       (9, 'Shorn Batbrood Cuffs', 'Wrist', '417 Agility', '397 Critical Strike', '1', 'Leather', 'No'), /* Black Rook Hold, Smashspite the Hateful */
+       (10, 'Felbat Leather Vest', 'Chest', '742 Agility', '670 Critical Strike', '1', 'Leather', 'No'), /* Black Rook Hold, Smashspite the Hateful */
+       (11, 'Mardum Chain Vest', 'Chest', '742 Intellect', '388 Versatility', '1', 'Mail', 'No') /* Black Rook Hold, Smashspite the Hateful */
 on conflict do nothing;
 
 insert into "set" (set_id, name, set_bonus)
-values ()
+values (1, 'robert', 'awesome')
 on conflict do nothing;
 
 insert into location (id, map_region)
@@ -91,75 +90,75 @@ on conflict do nothing;
 
 insert into dungeon (id, dungeon_name, boss)
 values (1, 'Black Rook Hold', 'Amalgam of Souls'),
-       (2, 'Black Rook Hold', 'Illysanna Ravencrest'),
-       (3, 'Black Rook Hold', 'Smashspite the Hateful'),
-       (4, 'Black Rook Hold', 'Lord Kur''talos Ravencrest'),
-       (5, 'Court of Stars', 'Patrol Captain Gerdo'),
-       (6, 'Court of Stars', 'Talixae Flamewreath'),
-       (7, 'Court of Stars', 'Adisor Melandrus'),
-       (8, 'Eye of Azshara', 'Serpentrix'),
-       (9, 'Eye of Azshara', 'Warlord Parjesh'),
-       (10, 'Eye of Azshara', 'Lady Hatecoil'),
-       (11, 'Eye of Azshara', 'King Deepbeard'),
-       (12, 'Eye of Azshara', 'Wrath of Azshara'),
-       (13, 'Halls of Valor', 'Hymdall'),
-       (14, 'Halls of Valor', 'Hyrja'),
-       (15, 'Halls of Valor', 'Fenryr'),
-       (16, 'Halls of Valor', 'God-King Skovald'),
-       (17, 'Halls of Valor', 'Odyn'),
-       (18, 'Dearkheart Thicket', 'Archdruid Glaidalis'),
-       (19, 'Dearkheart Thicket', 'Oakheart'),
-       (20, 'Dearkheart Thicket', 'Dresaron'),
-       (21, 'Dearkheart Thicket', 'Shade of Xavius'),
-       (22, 'Maw of Souls', 'Ymiron'),
-       (23, 'Maw of Souls', 'Harbaron'),
-       (24, 'Maw of Souls', 'Helya'),
-       (25, 'Neltharion''s Lair', 'Rokmora'),
-       (26, 'Neltharion''s Lair', 'Ularogg Cragshaper'),
-       (27, 'Neltharion''s Lair', 'Naraxas'),
-       (28, 'Neltharion''s Lair', 'Dargrul the Underking'),
-       (29, 'The Arcway', 'Ivanyr'),
-       (30, 'The Arcway', 'Corstilax'),
-       (31, 'The Arcway', 'General Xakal'),
-       (32, 'The Arcway', 'Nal''tira'),
-       (33, 'The Arcway', 'Advisor Vandros'),
-       (34, 'Vault of the Wardens', 'Tirathon Saltheril'),
-       (35, 'Vault of the Wardens', 'Ash''golm'),
-       (36, 'Vault of the Wardens', 'Glazer'),
-       (37, 'Vault of the Wardens', 'Cordana Felsong'),
-       (38, 'Vault of the Wardens', 'Inquisitor Tormentorum'),
-       (39, 'Violet Hold', 'Erekem'),
-       (40, 'Violet Hold', 'Moragg'),
-       (41, 'Violet Hold', 'Ichoron'),
-       (42, 'Violet Hold', 'Xevozz'),
-       (43, 'Violet Hold', 'Lavanthor'),
-       (44, 'Violet Hold', 'Zuramat the Obliterator'),
-       (45, 'Violet Hold', 'Cyanigosa')
+       (1, 'Black Rook Hold', 'Illysanna Ravencrest'),
+       (1, 'Black Rook Hold', 'Smashspite the Hateful'),
+       (1, 'Black Rook Hold', 'Lord Kur''talos Ravencrest'),
+       (1, 'Court of Stars', 'Patrol Captain Gerdo'),
+       (1, 'Court of Stars', 'Talixae Flamewreath'),
+       (1, 'Court of Stars', 'Adisor Melandrus'),
+       (1, 'Eye of Azshara', 'Serpentrix'),
+       (1, 'Eye of Azshara', 'Warlord Parjesh'),
+       (1, 'Eye of Azshara', 'Lady Hatecoil'),
+       (1, 'Eye of Azshara', 'King Deepbeard'),
+       (1, 'Eye of Azshara', 'Wrath of Azshara'),
+       (1, 'Halls of Valor', 'Hymdall'),
+       (1, 'Halls of Valor', 'Hyrja'),
+       (1, 'Halls of Valor', 'Fenryr'),
+       (1, 'Halls of Valor', 'God-King Skovald'),
+       (1, 'Halls of Valor', 'Odyn'),
+       (1, 'Dearkheart Thicket', 'Archdruid Glaidalis'),
+       (1, 'Dearkheart Thicket', 'Oakheart'),
+       (1, 'Dearkheart Thicket', 'Dresaron'),
+       (1, 'Dearkheart Thicket', 'Shade of Xavius'),
+       (1, 'Maw of Souls', 'Ymiron'),
+       (1, 'Maw of Souls', 'Harbaron'),
+       (1, 'Maw of Souls', 'Helya'),
+       (1, 'Neltharion''s Lair', 'Rokmora'),
+       (1, 'Neltharion''s Lair', 'Ularogg Cragshaper'),
+       (1, 'Neltharion''s Lair', 'Naraxas'),
+       (1, 'Neltharion''s Lair', 'Dargrul the Underking'),
+       (1, 'The Arcway', 'Ivanyr'),
+       (1, 'The Arcway', 'Corstilax'),
+       (1, 'The Arcway', 'General Xakal'),
+       (1, 'The Arcway', 'Nal''tira'),
+       (1, 'The Arcway', 'Advisor Vandros'),
+       (1, 'Vault of the Wardens', 'Tirathon Saltheril'),
+       (1, 'Vault of the Wardens', 'Ash''golm'),
+       (1, 'Vault of the Wardens', 'Glazer'),
+       (1, 'Vault of the Wardens', 'Cordana Felsong'),
+       (1, 'Vault of the Wardens', 'Inquisitor Tormentorum'),
+       (1, 'Violet Hold', 'Erekem'),
+       (1, 'Violet Hold', 'Moragg'),
+       (1, 'Violet Hold', 'Ichoron'),
+       (1, 'Violet Hold', 'Xevozz'),
+       (1, 'Violet Hold', 'Lavanthor'),
+       (1, 'Violet Hold', 'Zuramat the Obliterator'),
+       (1, 'Violet Hold', 'Cyanigosa')
 on conflict do nothing;
 
 insert into raid (id, raid_name, boss)
-values (1, 'Emerald Nightmare', 'Ursoc'),
+values (2, 'Emerald Nightmare', 'Ursoc'),
        (2, 'Emerald Nightmare', 'Nythendra'),
-       (3, 'Emerald Nightmare', 'Dragons of Nightmare'),
-       (4, 'Emerald Nightmare', 'Xavius'),
-       (5, 'Emerald Nightmare', 'I''lgynoth'),
-       (6, 'Emerald Nightmare', 'Elerethe Renferal'),
-       (7, 'Emerald Nightmare', 'Cenarius'),
-       (8, 'Trial of Valor', 'Odyn'),
-       (9, 'Trial of Valor', 'Guarm'),
-       (20, 'Trial of Valor', 'Helya'),
-       (11, 'The Nighthold', 'Skorpyron'),
-       (12, 'The Nighthold', 'Chronomatic Anomaly'),
-       (13, 'The Nighthold', 'Trilliax'),
-       (14, 'The Nighthold', 'Spellblade Aluriel'),
-       (16, 'The Nighthold', 'High Botanist Tel''am'),
-       (17, 'The Nighthold', 'Star Augur Etraeus')
-       (18, 'The Nighthold', 'Tichondrius'),
-       (19, 'The Nighthold', 'Krosus'),
-       (20, 'The Nighthold', 'Grand Magistrix Elisande'),
-       (21, 'The Nighthold', 'Gul''dan')
+       (2, 'Emerald Nightmare', 'Dragons of Nightmare'),
+       (2, 'Emerald Nightmare', 'Xavius'),
+       (2, 'Emerald Nightmare', 'I''lgynoth'),
+       (2, 'Emerald Nightmare', 'Elerethe Renferal'),
+       (2, 'Emerald Nightmare', 'Cenarius'),
+       (2, 'Trial of Valor', 'Odyn'),
+       (2, 'Trial of Valor', 'Guarm'),
+       (2, 'Trial of Valor', 'Helya'),
+       (2, 'The Nighthold', 'Skorpyron'),
+       (2, 'The Nighthold', 'Chronomatic Anomaly'),
+       (2, 'The Nighthold', 'Trilliax'),
+       (2, 'The Nighthold', 'Spellblade Aluriel'),
+       (2, 'The Nighthold', 'High Botanist Tel''am'),
+       (2, 'The Nighthold', 'Star Augur Etraeus'),
+       (2, 'The Nighthold', 'Tichondrius'),
+       (2, 'The Nighthold', 'Krosus'),
+       (2, 'The Nighthold', 'Grand Magistrix Elisande'),
+       (2, 'The Nighthold', 'Gul''dan')
 on conflict do nothing;
 
 insert into vendor (id, first_name, surname, price)
-values (1, 'casey', 'poole', '2')
+values (3, 'casey', 'poole', '2')
 on conflict do nothing;
