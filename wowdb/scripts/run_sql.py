5d699cdf362f52496156ed3bc64b5a3e612ffb3d
Aaron Schif <aschif@netdevgroup.com>
2016-11-29 21:01:40 -0600

    Update run sql

--- wowdb/scripts/run_sql.py
+++ wowdb/scripts/run_sql.py
@@ -3,7 +3,8 @@ from wowdb.sql import cursor, get_sql
 
 def run_sql():
     with cursor() as cur:
-        cur.execute(get_sql(input("Name of the sql file: ")))
+        cur.execute(get_sql('create_tables.sql'))
+        cur.execute(get_sql('populate_tables.sql'))
 
 if __name__ == '__main__':
     run_sql()
