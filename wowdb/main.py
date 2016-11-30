5f055756682f075a034e0ba927d9ff14a32fdb70
rclafferty <rclafferty@ksu.edu>
2016-11-30 02:51:11 -0600

    Added price functionality
    NOT WORKING:
    -- Multiple primary AND secondary stat lookup
    -- Secondary stat value comparison

--- wowdb/main.py
+++ wowdb/main.py
@@ -19,6 +19,9 @@ async def handle(request):
         pmax = request.GET.get('p_maximum', 10000)
         smax = request.GET.get('s_minimum', 1)
         smin = request.GET.get('s_maximum', 10000)
+        mat = request.GET.get('materials', 'ANY')
+        pr_min = request.GET.get('price_min', 0)
+        pr_max = request.GET.get('price_max', 999999999)
 
         s = "select * from gear where gear.name ilike '%%'||%s||'%%'"
         l = [search]
@@ -32,23 +35,30 @@ async def handle(request):
             s += " AND gear.slot ilike '%%'||%s||'%%'"
             l.append(slot)
 
-        # pstat does not work if there is more than one stat chosen... How do I fix this? (RCL)
+        # pstat does not work if there is more than one stat chosen... How do I fix this?
         if pstat != 'ANY':
             s += " AND gear.primary_stat ilike '%%'||%s||'%%'"
             l.append(pstat)
 
-        s += " AND gear.primary_stat_val >= %s"
+        s += " AND (gear.primary_stat_val >= %s"
         l.append(pmin)
 
-        s += " AND gear.primary_stat_val <= %s"
+        s += " AND gear.primary_stat_val <= %s)"
         l.append(pmax)
 
+        # THIS WORKS
+        # s += " AND gear.sec_stat_1_val >= 300"
+
+        # THIS DOES NOT
+        # s += " AND gear.sec_stat_1_val >= %s"
+        # l.append(smin)
+
         # Don't think this one works... HELP
-        # if sstat != 'ANY':
-        #    s += " AND (gear.secondary_stat_1 ilike '%%'||%s||'%%'"
-        #    l.append(sstat)
-        #    s += " OR gear.secondary_stat_2 ilike '%%'||%s||'%%')"
-        #    l.append(sstat)
+        if sstat != 'ANY':
+            s += " AND (gear.secondary_stat_1 ilike '%%'||%s||'%%'"
+            l.append(sstat)
+            s += " OR gear.secondary_stat_2 ilike '%%'||%s||'%%')"
+            l.append(sstat)
 
         # s += " AND gear.sec_stat_1_val >= %s"
         # l.append(smin)
@@ -62,21 +72,19 @@ async def handle(request):
         #    s += " AND gear.sec_stat_2_val <= %d"
         #    l.append(smax)
 
+        if mat != 'ANY':
+            s += " AND gear.material ilike '%%'||%s||'%%'"
+            l.append(mat)
+
+        if int(pr_min) > 0:
+            s += " AND gear.price >= %s"
+            l.append(pr_min)
+
+            s += " AND gear.price <= %s"
+            l.append(pr_max)
+
         cur.execute(s, l)
 
-        #if slot == 'ANY':
-        #    if tradable == 'Both':
-        #        cur.execute("select * from gear where gear.name ilike '%%'||%s||'%%'", [search])
-        #    else:
-        #        tradable = tradable == 'True'
-        #        cur.execute("select * from gear where gear.name ilike '%%'||%s||'%%' AND gear.tradable is %s", [search, tradable])
-        #else:
-        #    if tradable == 'Both':
-        #        cur.execute("select * from gear where gear.name ilike '%%'||%s||'%%' AND gear.slot ilike '%%'||%s||'%%'", [search, slot])
-        #    else:
-        #        tradable = tradable == 'True'
-        #        cur.execute("select * from gear where gear.name ilike '%%'||%s||'%%' AND gear.tradable is %s AND gear.slot ilike '%%'||%s||'%%'", [search, tradable, slot])
-        #cur.execute("select * from gear where gear.name ilike '%%'||%s||'%%'", [search])
         gears = cur.fetchall()
     return dict(gears=gears)
 
