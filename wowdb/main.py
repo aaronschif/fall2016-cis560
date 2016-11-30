48f47fe1b0d1f1c3ada316d06bd5988fc22bfe47
Aaron Schif <aschif@netdevgroup.com>
2016-11-29 20:54:47 -0600

    Aiohttp

--- wowdb/main.py
+++ wowdb/main.py
@@ -1,29 +1,28 @@
 from pathlib import Path
 
-import cherrypy
-from jinja2 import Environment, PackageLoader
+from aiohttp import web
+import aiohttp_jinja2
+import jinja2
 
 from .sql import cursor
 
-env = Environment(loader=PackageLoader(__package__, 'templates'))
+
+@aiohttp_jinja2.template('index.html')
+async def handle(request):
+    with cursor() as cur:
+        search = request.GET.get('search')
+        cur.execute(
+            "select * from gear where gear.name ilike '%%'||%s||'%%'", [search])
+        gears = cur.fetchall()
+    return dict(gears=gears)
 
 
-class Root:
-    @cherrypy.expose
-    def index(self, search=""):
-        with cursor() as cur:
-            cur.execute("select * from gear where gear.name ilike '%%'||%s||'%%'", [search])
-            gears = cur.fetchall()
-        tmpl = env.get_template('index.html')
-        return tmpl.render(gears=gears)
-
-conf = {'/static': {
-    'tools.staticdir.on': True,
-    'tools.staticdir.dir': str(Path(__file__).parent/'static'),
-    'tools.staticdir.content_types': {
-        'rss': 'application/xml',
-        'atom': 'application/atom+xml'}}}
+app = web.Application(debug=True)
+app.router.add_get('/', handle)
+app.router.add_static('/static/', path=str(Path(__file__).parent/'static'), name='static')
+aiohttp_jinja2.setup(
+    app, loader=jinja2.PackageLoader('wowdb', 'templates'))
 
 
 def main():
-    cherrypy.quickstart(Root(), config=conf)
+    web.run_app(app)
