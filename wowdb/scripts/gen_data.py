3e74e4de5776093ecf541648dc9912f8fafe94d6
Aaron Schif <aschif@netdevgroup.com>
2016-12-06 21:13:04 -0600

    New generated data

--- wowdb/scripts/gen_data.py
+++ wowdb/scripts/gen_data.py
@@ -1,10 +1,21 @@
 import csv
 from pathlib import Path
+from random import choice
 
 from faker import Faker
 fake = Faker()
 
 LOCATION_NAMESPACE = (1000, 2000)
+POSSIBLE_LOCATIONS = (
+    "highmountain",
+    "stormheim",
+    "azsuna",
+    "val'sharah",
+    "suramar",
+    "dalaran",
+    "the broken shore",
+)
+
 location_file = Path(__file__).parent.parent/'sql'/'location.csv'
 vendor_file = Path(__file__).parent.parent/'sql'/'vendor.csv'
 
@@ -14,7 +25,7 @@ def main():
         location_writer = csv.writer(location_fd)
         vendor_writer = csv.writer(vendor_fd)
         for i in range(*LOCATION_NAMESPACE):
-            location_writer.writerow([i, '{} of {}'.format(fake.street_name(), fake.state())])
+            location_writer.writerow([i, 'Vendor', choice(POSSIBLE_LOCATIONS)])
             vendor_writer.writerow([i, fake.first_name(), fake.last_name()])
 
 
